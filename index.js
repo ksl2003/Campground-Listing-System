ff9c7309c8451e3ad5f37916d71436336ae31ce2
ksl2003 <ksuryalakshman5@gmail.com>
2025-11-20 23:10:19 +0530

    Solved Google Auth Problem

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://campground-listing-system.onrender.com/auth/google/callback",
+        process.env.GOOGLE_CALLBACK_URL ||
+        "http://localhost:3000/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       try {
